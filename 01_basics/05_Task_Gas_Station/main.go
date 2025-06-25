b5a42605ffeb10fce6dac1c5442caacc95055f54
8alex2345 <ssochrannyy@email.com>
2025-06-25 22:58:44 +0300

    Финал задачи на Автозаправочную станцию

--- 01_basics/05_Task_Gas_Station/main.go
+++ 01_basics/05_Task_Gas_Station/main.go
@@ -26,19 +26,17 @@ func NewCar(name string, capacity, fuelLevel int) *Car {
 }
 
 type GasSstationImpl interface {
-	RefuelCar(shortage int, liter int)
+	RefuelCar(liter int)
 	GetFuelGaz(gas *GasStation) int
 }
 
-func (c *Car) RefuelCar(shortage, liter int) {
-	shortage = c.capacity - c.fuelLevel
-	fmt.Println("До полного бака не хватает - ", shortage, "Введите количество которое хотите заправить - ", liter)
-	if shortage < liter || liter < 1 {
-		fmt.Println("Вы ввели недопустимое количество")
+func (c *Car) RefuelCar(liter int) {
+	if c.fuelLevel+liter > c.capacity {
+		c.fuelLevel = c.capacity
+		fmt.Println("Бак заполнен")
+	} else {
+		c.fuelLevel += liter
 	}
-	c.fuelLevel += liter
-	fmt.Println("Заправка закончена, залито в бак - ", liter)
-
 }
 func (g *GasStation) GetFuelGaz(liter int) bool {
 	if g.amountFuel < liter {
@@ -50,10 +48,19 @@ func (g *GasStation) GetFuelGaz(liter int) bool {
 	return true
 }
 
-func Refueling(station *GasStation, car *Car, liter int) {
-	if car.fuelLevel < liter {
-
+func CarRefuelingGas(station *GasStation, car *Car, liter int) {
+	shortage := car.capacity - car.fuelLevel
+	fmt.Println("До полного бака не хватает - ", shortage, "Введите количество которое хотите заправить - ", liter)
+	if shortage < liter || liter < 1 {
+		fmt.Println("Вы ввели недопустимое количество")
 	}
+	if station.GetFuelGaz(liter) {
+		car.RefuelCar(liter)
+		fmt.Println("Заправка закончена, залито в бак - ", liter)
+	} else {
+		fmt.Println("На заправке недостоточно топлива")
+	}
+
 }
 
 func main() {
@@ -61,6 +68,6 @@ func main() {
 	car := NewCar("Nissan", 100, 40)
 	var liter int
 	fmt.Scan(&liter)
-	Refueling(gasStation, car, liter)
+	CarRefuelingGas(gasStation, car, liter)
 
 }
