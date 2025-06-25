b27acaf958fd680341771ba8443c1a163ce4dfab
8alex2345 <ssochrannyy@email.com>
2025-06-25 23:14:49 +0300

    Review Автозаправка

---
+++ 01_basics/reviwe/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Car struct {
+	name      string
+	capacity  int
+	fuelLevel int
+}
+type GasStation struct {
+	amountFuel  int
+	capacityGas int
+}
+
+func NewCar(name string, capacity, fuelLevel int) *Car {
+	if fuelLevel < 0 || capacity < 0 {
+		fmt.Println("Введены отрицательные числа")
+	}
+	return &Car{
+		name:      name,
+		capacity:  capacity,
+		fuelLevel: fuelLevel,
+	}
+}
+
+type GasSstationImpl interface {
+	RefuelCar(liter int)
+	GetFuelGaz(liter int) bool
+}
+
+func (c *Car) RefuelCar(liter int) {
+	if c.fuelLevel+liter > c.capacity {
+		c.fuelLevel = c.capacity
+		fmt.Println("Бак заполнен")
+	} else {
+		c.fuelLevel += liter
+	}
+}
+func (g *GasStation) GetFuelGaz(liter int) bool {
+	if g.amountFuel < liter {
+		fmt.Println("Заправка закрыта, нет бензина")
+		return false
+	}
+	g.capacityGas -= liter
+	fmt.Println("Топливо есть, можно заправляться")
+	return true
+}
+
+func CarRefuelingGas(station *GasStation, car *Car, liter int) {
+	shortage := car.capacity - car.fuelLevel
+	fmt.Println("До полного бака не хватает - ", shortage, "Введите количество которое хотите заправить - ", liter)
+	if shortage < liter || liter < 1 {
+		fmt.Println("Вы ввели недопустимое количество")
+	}
+	if station.GetFuelGaz(liter) {
+		car.RefuelCar(liter)
+		fmt.Println("Заправка закончена, залито в бак - ", liter)
+	} else {
+		fmt.Println("На заправке недостоточно топлива")
+	}
+
+}
+
+func main() {
+	gasStation := &GasStation{capacityGas: 100000, amountFuel: 50000}
+	car := NewCar("Nissan", 100, 40)
+	var liter int
+	fmt.Scan(&liter)
+	CarRefuelingGas(gasStation, car, liter)
+
+}
