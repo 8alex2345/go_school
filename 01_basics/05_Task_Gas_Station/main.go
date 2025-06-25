2d9ad7d9b013353ff1efa863cb000368f27909a8
8alex2345 <ssochrannyy@email.com>
2025-06-25 22:06:58 +0300

    Изменение по задаче на автозаправку

--- 01_basics/05_Task_Gas_Station/main.go
+++ 01_basics/05_Task_Gas_Station/main.go
@@ -1,41 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Car struct {
-	Name      string
-	Capacity  int
-	FuelLevel int
+	name      string
+	capacity  int
+	fuelLevel int
 }
-
 type GasStation struct {
-	refuel int
-	status string
-}
-type GasSstationImpl interface {
-	refuelCar(car *Car) string
+	amountFuel  int
+	capacityGas int
 }
 
-func (g *GasStation) refuelCar(car *Car) string {
-	if car.Capacity > car.FuelLevel {
-		g.refuel = car.Capacity - car.FuelLevel
-		car.FuelLevel += g.refuel
-		g.status = "Идет заправка"
-		return g.status
+func NewCar(name string, capacity, fuelLevel int) *Car {
+	if fuelLevel < 0 || capacity < 0 {
+		fmt.Println("Введены отрицательные числа")
 	}
+	return &Car{
+		name:      name,
+		capacity:  capacity,
+		fuelLevel: fuelLevel,
+	}
+}
 
-	return "Машина заправлена"
+type GasSstationImpl interface {
+	RefuelCar(shortage int, liter int)
+	GetFuelGaz(gas *GasStation) int
+}
+
+func (c *Car) RefuelCar(shortage, liter int) {
+	shortage = c.capacity - c.fuelLevel
+	fmt.Println("До полного бака не хватает - ", shortage, "Введите количество которое хотите заправить - ", liter)
+	if shortage < liter || liter < 1 {
+		fmt.Println("Вы ввели недопустимое количество")
+	}
+	c.fuelLevel += liter
+	fmt.Println("Заправка закончена, залито в бак - ", liter)
 
 }
+func (g *GasStation) GetFuelGaz(liter int) bool {
+	if g.amountFuel < liter {
+		fmt.Println("Заправка закрыта, нет бензина")
+		return false
+	}
+	g.capacityGas -= liter
+	fmt.Println("Топливо есть, можно заправляться")
+	return true
+}
+
+func Refueling(station *GasStation, car *Car, liter int) {
+	if car.fuelLevel < liter {
+
+	}
+}
+
 func main() {
-	car1 := &Car{Name: "Nissan", Capacity: 100, FuelLevel: 40}
-	gasStation := &GasStation{}
-	status := gasStation.refuelCar(car1)
-	fmt.Println(status)
-
-	fmt.Println("Уровеь топлива - ", car1.FuelLevel)
-
-	status = gasStation.refuelCar(car1)
-	fmt.Println(status)
+	gasStation := &GasStation{capacityGas: 100000, amountFuel: 50000}
+	car := NewCar("Nissan", 100, 40)
+	var liter int
+	fmt.Scan(&liter)
+	Refueling(gasStation, car, liter)
 
 }
