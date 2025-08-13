b0923b4baafc38c6c9e96b40a8ddbf75207af340
8alex2345 <ssochrannyy@email.com>
2025-08-13 20:16:51 +0300

    Задача на батчи с контекстом иВГ

--- 01_basics/08_task_batch_processing/main.go
+++ 01_basics/08_task_batch_processing/main.go
@@ -3,51 +3,79 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
+	var wg sync.WaitGroup
 	input := make(chan int)
-	go StartBatchProcessor(ctx, input)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	wg.Add(1)
+	go StartBatchProcessor(ctx, input, &wg)
 	go func() {
-		for i := 1; i <= 20; i++ {
-			input <- i
+		defer wg.Done()
+
+		for i := 0; i <= 20; i++ {
+			select {
+			case input <- i:
+				fmt.Println("отправялем в канал число - ", i)
+			case <-ctx.Done():
+				fmt.Println("конеткст отменен. прекращаем отправку")
+				return
+			}
 		}
+		close(input)
+
 	}()
-	<-ctx.Done()
+	wg.Wait()
 	fmt.Println("Main: processing stopped")
-
 }
-
-func StartBatchProcessor(ctx context.Context, input <-chan int) {
+func StartBatchProcessor(ctx context.Context, input <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	batchSize := 5
 	batch := make([]int, 0, batchSize)
 	timer := time.NewTimer(2 * time.Second)
+	defer timer.Stop()
+
 	for {
 		select {
-		case <-ctx.Done():
-			fmt.Println("контекст отменен")
-			return
-		case value := <-input:
+		case value, ok := <-input:
+			if !ok {
+				if len(batch) > 0 {
+					fmt.Println("канал закрыт, обрабатываем последний батч. - ", batch)
+
+				} else {
+					fmt.Println("Processed batch:", batch)
+				}
+				return
+			}
 			batch = append(batch, value)
 			if len(batch) == batchSize {
 				fmt.Println("Processed batch:", batch)
 				batch = batch[:0]
-			}
-			if !timer.Stop() {
-				<-timer.C
-				timer.Reset(2 * time.Second)
-			}
-		case <-timer.C:
-			if len(batch) > 0 {
-				fmt.Println("Processed batch (timeout):", batch)
-				batch = batch[:0]
+				if !timer.Stop() {
+					<-timer.C
+				}
 				timer.Reset(2 * time.Second)
 			}
 
+		case <-timer.C:
+			if len(batch) > 0 {
+				fmt.Println("Processed batch:", batch)
+				batch = batch[:0]
+			}
+			timer.Reset(2 * time.Second)
+
+		case <-ctx.Done():
+			fmt.Println("контекст отменен")
+			if len(batch) > 0 {
+				fmt.Println("Processed batch:", batch)
+			}
+			return
+
 		}
+
 	}
 }
