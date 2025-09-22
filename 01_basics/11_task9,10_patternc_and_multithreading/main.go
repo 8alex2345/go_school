020458a6dd24e11f31f8a65f779c4fd51d5776be
8alex2345 <ssochrannyy@email.com>
2025-09-22 17:58:24 +0300

    задача 9,10 FANin FANout

--- 01_basics/11_task9,10_patternc_and_multithreading/main.go
+++ 01_basics/11_task9,10_patternc_and_multithreading/main.go
@@ -1,12 +1,30 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	cannalMain := make(chan int)
+	go func() {
+		defer close(cannalMain)
+		for i := 0; i < 10; i++ {
+			cannalMain <- i
+		}
+
+	}()
+	n := 2
+	channals := Split(cannalMain, n)
+	for i := 0; i < 10; i++ {
+		for _, ch := range channals {
+			fmt.Println(<-ch)
+		}
+	}
 
 }
 func Merge(cs ...<-chan int) <-chan int {
-	out := make(chan int)
+	out := make(chan int, len(cs))
 	var wg sync.WaitGroup
 
 	go func() {
@@ -24,4 +42,35 @@ func Merge(cs ...<-chan int) <-chan int {
 	}()
 
 	return out
+}
+
+func Split(ch <-chan int, n int) []<-chan int {
+	chSplit := make([]chan int, n)
+
+	for i := 0; i < n; i++ {
+		chSplit[i] = make(chan int)
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		idx := 0
+		for value := range ch {
+			chSplit[idx] <- value
+			idx = (idx + 1) % n
+		}
+	}()
+	go func() {
+		wg.Wait()
+		for _, c := range chSplit {
+			close(c)
+		}
+	}()
+	resultChs := make([]<-chan int, n)
+	for i := 0; i < n; i++ {
+		resultChs[i] = chSplit[i]
+	}
+
+	return resultChs
+
 }
